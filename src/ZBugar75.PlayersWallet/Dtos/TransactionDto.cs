41d15adc9ee1c35f493736ba3166c3983a21778f
Zoltan Bugar <zoltan.bugar@dataformers.at>
2021-05-14 00:35:10 +0200

    Creating controller basis

---
+++ src/ZBugar75.PlayersWallet/Dtos/TransactionDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Zbugar75.PlayersWallet.Api.Dtos
+{
+    public class TransactionDto
+    {
+        public Guid id { get; set; }
+        public decimal amount { get; set; }
+    }
+}
