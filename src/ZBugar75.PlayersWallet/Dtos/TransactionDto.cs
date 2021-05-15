5d8c7e06d42e8fcea623dc29fa6972f09bb5d94d
Zoltan Bugar <zoltan.bugar@dataformers.at>
2021-05-15 09:26:11 +0200

    TransactionTypeDto created

--- src/ZBugar75.PlayersWallet/Dtos/TransactionDto.cs
+++ src/ZBugar75.PlayersWallet/Dtos/TransactionDto.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using Zbugar75.PlayersWallet.Api.Dtos.Enums;
 
 namespace Zbugar75.PlayersWallet.Api.Dtos
 {
     public class TransactionDto
     {
-        public Guid id { get; set; }
-        public decimal amount { get; set; }
+        public Guid Id { get; set; }
+
+        public TransactionTypeDto TransactionType { get; set; }
+
+        public decimal Amount { get; set; }
     }
 }
