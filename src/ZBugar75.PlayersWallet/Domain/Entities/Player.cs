9476ee802546909240fb44fd97f33c9472e807af
Zoltan Bugar <zoltan.bugar@dataformers.at>
2021-05-15 10:52:26 +0200

    Player reference added to Transaction

--- src/ZBugar75.PlayersWallet/Domain/Entities/Player.cs
+++ src/ZBugar75.PlayersWallet/Domain/Entities/Player.cs
@@ -5,5 +5,7 @@
         public string Username { get; set; }
 
         public Wallet Wallet { get; set; }
+
+        public Transaction Transaction { get; set; }
     }
 }
--- src/ZBugar75.PlayersWallet/Domain/Entities/Transaction.cs
+++ src/ZBugar75.PlayersWallet/Domain/Entities/Transaction.cs
@@ -1,9 +1,12 @@
-﻿using Zbugar75.PlayersWallet.Api.Domain.Entities.Enums;
+﻿using System;
+using Zbugar75.PlayersWallet.Api.Domain.Entities.Enums;
 
 namespace Zbugar75.PlayersWallet.Api.Domain.Entities
 {
     public class Transaction : EntityBase
     {
+        public Guid PlayerId { get; set; }
+
         public TransactionType TransactionType { get; set; }
 
         public decimal Amount { get; set; }
