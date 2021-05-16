c6e97d7824c5ac66cb18d21f8793294dacb94e7e
Zoltan Bugar <zoltan.bugar@dataformers.at>
2021-05-16 21:12:02 +0200

    Redesigning exceptions

---
+++ src/ZBugar75.PlayersWallet.Api/Common/Exceptions/LowBalanceException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Zbugar75.PlayersWallet.Api.Common.Exceptions
+{
+    [Serializable]
+    public class LowBalanceException : Exception
+    {
+        public LowBalanceException() { }
+
+        public LowBalanceException(string message)
+            : base(message) { }
+
+        public LowBalanceException(string message, Exception inner)
+            : base(message, inner) { }
+
+        protected LowBalanceException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext)
+        {
+        }
+    }
+}
